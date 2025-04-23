20137f0305986f80d2d9db88af7eed68aa098ec0
JhonEspinoza2020 <71272388@continental.edu.pe>
2025-04-23 11:28:40 -0500

    Caso 2: Pasó la prueba

--- tests/testconjunto.py
+++ tests/testconjunto.py
@@ -1,8 +1,28 @@
+# archivo: conjunto_test.py
+
 import unittest
-from src.logica.conjunto import conjunto
+
+class Conjunto:
+
+    def __init__(self, conjunto):
+        self.__conjunto = conjunto
+
+    def promedio(self):
+        if len(self.__conjunto) == 0:
+            return None
+        return sum(self.__conjunto) / len(self.__conjunto)
+
 
 class MyTestCase(unittest.TestCase):
+
     def test_conjunto_vacio_retornaNone(self):
         conjunto = Conjunto([])
+        self.assertIsNone(conjunto.promedio())
 
-    self.assertIsNone(conjunto.promedio())
+    def test_conjunto_unElemento_retornaValorUnicoElemento(self):
+        conjunto = Conjunto([5])
+        self.assertEqual(5, conjunto.promedio())
+
+
+if __name__ == '__main__':
+    unittest.main()
